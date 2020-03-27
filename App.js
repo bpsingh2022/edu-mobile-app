6859aa0fc014a42f2a4604371f71a0195ceec571
bobby singh <bobbysingh2005@gmail.com>
2020-03-27 22:20:33 +0530

    add navigation

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+return (
+<View style={styles.container}>
+<ScrollView>
+  <Text>test1</Text>
+  {for(let i=0; i<20; i++;) (
+  <Text>
+  किस्मत की कुंडली
+सीरियल क्वीन
+Appears to say: REPUBLIC.
+  
+© 2020 Republic. All rights reserved.
+Appears to say: R
+Don't miss another update from Republic World
+You can unsubscribe from notifications anytime.
+AllowLater
+Powered by iZooto
+
+  </Text>
+  )}
+</ScrollView>
+</View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+alignItems: 'center',
+justifyContent: 'center',
+},
 });
